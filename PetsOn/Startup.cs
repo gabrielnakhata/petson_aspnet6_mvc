f4dfe63a91f3c0aafb70dbdbe5396baa58a22356
Gabriel Nakhata <gabrielnakata@gmail.com>
2022-10-08 18:41:25 -0300

    add my class Startup in project AspNet6

--- PetsOn/Startup.cs
+++ PetsOn/Startup.cs
@@ -2,5 +2,37 @@
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+
+        }
+
+        public IConfiguration Configuration { get;}
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
     }
 }
